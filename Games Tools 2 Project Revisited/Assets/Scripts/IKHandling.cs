ed275edfc83932efd3de90232f29a5c805f1cca7
will3600 <william.hulshof@mydit.ie>
2018-12-06 22:35:31 +0000

    Tweaks and Changes
    Failed attempt at implemting IK mohand movements.

---
+++ Games Tools 2 Project Revisited/Assets/Scripts/IKHandling.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class IKHandling : MonoBehaviour {
+
+    Animator anim;
+
+    public float ikWeight = 1;
+
+    public Transform leftIKTarget;
+    public Transform rightIKTarget;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnAnimatorIK()
+    {
+        anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, ikWeight);
+        anim.SetIKPositionWeight(AvatarIKGoal.RightHand, ikWeight);
+
+        anim.SetIKPosition(AvatarIKGoal.LeftHand, leftIKTarget.position);
+        anim.SetIKPosition(AvatarIKGoal.RightHand, rightIKTarget.position);
+    }
+
+}
