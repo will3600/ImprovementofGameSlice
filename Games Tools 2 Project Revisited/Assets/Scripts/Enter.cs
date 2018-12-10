c436e795eaf2f74da3a1a3f4e2b77fa279faad7b
will3600 <william.hulshof@mydit.ie>
2018-12-10 15:29:15 +0000

    Tweaks and Changes
    Added Lights to the course that light up when when the player passes a collider. The Lights indicate the difficulty of the current prefab you are walking on.

---
+++ Games Tools 2 Project Revisited/Assets/Scripts/Enter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enter : MonoBehaviour
+{
+    public GameObject SphereLeft;
+    public GameObject SphereRight;
+
+    private void OnTriggerEnter()
+    {
+        SphereLeft.SetActive(true);
+        SphereRight.SetActive(true);
+    }
+}	
